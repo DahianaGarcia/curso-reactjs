f9f872d34675adf8057b066c882f37ec79b81511
Dahiana <diseno.desarollo@htg.com.co>
2023-01-23 15:18:23 -0500

    se subio proyecto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import  imageRickMorty from "./img/rick-morty.png";
 import './App.css';
+import {useState} from "react";
+import Characters from "./components/Characters";
 
 function App() {
+  const [characters,setCharacters]=useState(null);
+  const reqApi=async()=>{
+    const api=await fetch("https://rickandmortyapi.com/api/character")
+    const characterApi=await api.json();
+    setCharacters(characterApi.results);
+    //console.log(characters);
+   // console.log("clicking");
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <h1 className="title">Rick y Morty</h1>
+       
+        {characters ? (
+       /*{filtramos si la constante characters tiene datos vamos a mostrarlo y si no tiene datos no lo muestre.
+         se va a hacer por medio de los ternarios}*/
+        <Characters characters={characters}setCharacters={setCharacters} />
+        ) : (
+       <>
+        
+      <img src={imageRickMorty}alt="Rick y Morty"className="img-home"></img>
+        <button onClick={reqApi}className="btn-search">Buscar Personajes</button>
+        </>
+        )}
       </header>
     </div>
   );
